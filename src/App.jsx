164023c2896edb22ab694c186a066c86877ba749
leonardovalealves <ra53673413813@acaonsfatima.org.br>
2025-10-08 10:18:57 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import './App.css'
+
+function App() {
+
+  return (
+   <div>
+    <header>
+<img src="https://media.discordapp.net/attachments/1399878782134780027/1425106718324428840/baixados_1.png?ex=68e6618e&is=68e5100e&hm=6c35417df49cc3e6c84880b695a1d805717ebe53c9ac72cfe2a6cec8f5ae25db&=&format=webp&quality=lossless" alt="Logo" height={'100px'}/>
+<div className='meio'>
+<button>Início</button>
+<button>Sobre Nós</button>
+</div>
+<button>Ajude-nos</button>
+    </header>
+ <br />
+
+<div className='meiozin'>
+
+
+  <div className="imagem-container">
+      <img
+        src="https://media.discordapp.net/attachments/1399878782134780027/1425442759992672377/image_4.png?ex=68e79a85&is=68e64905&hm=2b54778fe2df20d5bd0ecf906db9514b8d5b43dbf7b63f816e57c0be0c7b5a2e&=&format=webp&quality=lossless"
+        alt=""
+        className="imagem"
+      />
+      <div className="texto-sobre">Quer <div className='azul'>conhecer</div> outros <br />esportes que valem apena <br />ser <div className='azul'>praticados?</div></div>
+      <div className='botao-sobre'><button>Outros esportes <br />legais para <br /> se praticar!</button></div>
+    </div>
+<h2>Esporte, lazer e <br />restauração é a <br />nossa sigla e nosso <br /> dever com você!</h2>
+</div>
+<h2>Por um Brasil mais saudável e atlético!</h2>
+  <br />
+  <br />
+  <div>
+    <button></button>
+    <button></button>
+    <button></button>
+  </div>
+   </div>
+  )
+}
+
+export default App
