cf018fe52981c824ac813abe200ff51b38415919
leonardovalealves <ra53673413813@acaonsfatima.org.br>
2025-10-09 10:59:41 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
 
   <div className="imagem-container">
       <img
-        src="https://media.discordapp.net/attachments/1399878782134780027/1425442759992672377/image_4.png?ex=68e79a85&is=68e64905&hm=2b54778fe2df20d5bd0ecf906db9514b8d5b43dbf7b63f816e57c0be0c7b5a2e&=&format=webp&quality=lossless"
+        src="https://media.discordapp.net/attachments/1399878782134780027/1425442759992672377/image_4.png?ex=68e8ec05&is=68e79a85&hm=370989635e570e8c486deb316a186caf25685ec0563b74fa8ad49206a6d525d1&=&format=webp&quality=lossless"
         alt=""
         className="imagem"
       />
@@ -30,12 +30,108 @@ function App() {
 </div>
 <h2>Por um Brasil mais saudável e atlético!</h2>
   <br />
-  <br />
-  <div>
-    <button></button>
-    <button></button>
-    <button></button>
+  <div className="botoes-container">
+  <div className='botao1'>
+    <h3>Gráficos</h3>
+    <button>
+      <h4>Como estão as quadras <br /> e parques em SP!</h4>
+      <img src="https://media.discordapp.net/attachments/1399878782134780027/1425830689168822272/grafico-removebg-preview.png?ex=68e903ce&is=68e7b24e&hm=9e68ce0b445076874506f3064a1fb68de9bfc17d772a4cf97054071fefffa9c5&=&format=webp&quality=lossless" alt="" height={'50px'}/>
+    </button>
   </div>
+
+  <div className='botao2'>
+    <h3>Galeria</h3>
+    <button>
+      <h4>Veja os lugares que <br />já restauramos!</h4>
+      <img src="https://media.discordapp.net/attachments/1399878782134780027/1425830688799854693/galeria-removebg-preview.png?ex=68e903ce&is=68e7b24e&hm=73ab32b870ec7ac4c5b74fd258ac18b295d8fa8cd8b11953b4818db333303306&=&format=webp&quality=lossless" alt="" height={'50px'}/>
+    </button>
+  </div>
+
+  <div className='botao3'>
+    <h3>Agenda</h3>
+    <button>
+      <h4>Faça sua própria <br />agenda de exercícios!</h4>
+      <img src="https://media.discordapp.net/attachments/1399878782134780027/1425830639059341322/agenda-removebg-preview.png?ex=68e903c3&is=68e7b243&hm=5d40036438ca13bdaa61eaae9368ce2f7310cdfded97e4fbacc46133f359a143&=&format=webp&quality=lossless" alt="" height={'45px'}/>
+    </button>
+  </div>
+</div>
+
+ <div className='todo'>
+  <h1>Por que práticar exercícios físicos?</h1>
+
+  <div className='texto1'>
+<h3>
+ Benefícios Físicos
+ </h3>
+ <ul>
+  <li>
+    Fortalece músculos e ossos: exercícios de força e impacto ajudam na <br />
+       resistência física e previnem doenças como osteoporose.
+  </li>
+  <li>
+        Melhora o sistema cardiovascular: o coração trabalha de forma mais <br />
+       eficiente, reduzindo riscos de doenças cardíacas.
+  </li>
+   <li>
+Controla o peso corporal: aumenta o gasto calórico e ajuda na queima <br />
+       de gordura.
+  </li>
+  <li>
+    Aumenta a imunidade: o organismo fica mais resistente a doenças.
+  </li>
+  <li>
+    Melhora a postura e a coordenação motora: fortalece músculos <br />
+       estabilizadores e aumenta o equilíbrio.
+  </li>
+ </ul>
+
+  </div>
+
+  <div className='texto2'>
+    <h3>
+Benefícios Mentais e Emocionais
+    </h3>
+<ul>
+  <li>
+Reduz o estresse e a ansiedade: durante a prática, o corpo libera <br />
+       endorfinas, hormônios que promovem sensação de bem-estar.
+  </li>
+  <li>
+    Melhora o humor: ajuda a combater sintomas de depressão <br />
+       e aumenta a autoestima.
+  </li>
+  <li>
+    Aumenta a concentração e a memória: o fluxo de sangue para <br />
+       o cérebro melhora, favorecendo o raciocínio e o aprendizado.
+  </li>
+  <li>
+    Promove o sono de qualidade: quem se exercita tende a dormir <br />
+       melhor e mais profundamente.
+  </li>
+</ul>
+  </div>
+
+  <div className='texto3'>
+<h3>
+  Benefícios a Longo Prazo
+</h3>
+<ul>
+  <li>
+    Previne doenças crônicas: como diabetes tipo 2, hipertensão <br />
+       e obesidade.
+  </li>
+  <li>
+    Aumenta a longevidade: pessoas ativas tendem a viver mais <br />
+       e com melhor qualidade de vida.
+  </li>
+  <li>
+    Melhora a autonomia na velhice: mantém força e equilíbrio, <br />
+       reduzindo o risco de quedas e dependência.
+  </li>
+</ul>
+  </div>
+  <img src="https://media.discordapp.net/attachments/1399878782134780027/1425843121056645140/tcc_correadoraimg_2.png?ex=68e90f62&is=68e7bde2&hm=0928dfe6848113c91e83a6136d5f3699d97c73eb0de4af5ec22e20236ded2488&=&format=webp&quality=lossless&width=325&height=548" alt="" />
+ </div>
    </div>
   )
 }
